7976e3c72b0a682a2e31d874bc79bd5923713de4
ABHISHEK SUDHIR <abhisheksudhir2000@gmail.com>
2020-02-28 21:52:23 +0530

    links

---
+++ src/components/ExercisesList.js
@@ -0,0 +1,13 @@
+  
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+
+export default class ExercisesList extends Component{
+    render() {
+        return(
+            <div>
+                <p>ExercisesList</p>
+            </div>
+        )
+    }
+}
