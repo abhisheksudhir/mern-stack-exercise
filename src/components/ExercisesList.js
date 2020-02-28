cd4de79f27d68b1a1b20f8da4107c53351641837
ABHISHEK SUDHIR <abhisheksudhir2000@gmail.com>
2020-02-28 23:16:01 +0530

    creating user page

--- src/components/ExercisesList.js
+++ src/components/ExercisesList.js
@@ -1,6 +1,5 @@
-  
 import React, { Component } from 'react';
-import { Link } from 'react-router-dom';
+// import { Link } from 'react-router-dom';
 
 export default class ExercisesList extends Component{
     render() {
